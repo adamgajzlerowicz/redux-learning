a14ca0062870f09d2b8cecc4d3fa012c7888954d
Adam Gajzlerowicz <adam.gajzlerowicz@performancehorizon.com>
2016-09-26 07:34:37 +0200

    Before refactor

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,8 @@ import { World, Foo } from "./World";
 import expect from 'expect';
 import {counter, newCounter, todoApp} from "./reducers";
 import {createStore, combineReducers} from 'redux';
-
+const { Component } = React;
+let nextTodoId = 0;
 const store = createStore(
     todoApp
 );
@@ -19,25 +20,89 @@ var Counter = ({ state, onIncrement, onDecrement }) => {
     )
 }
 
-var TodoList = ({state}) => {
+const FilterLink = ({ filter, children, currentFilter }) => {
+    if (filter === currentFilter) {
+        return <span>{children}</span>;
+    }
     return (
-        <div>
-            list blah
-        </div>
+        <a href="#" onClick={ e => {
+            e.preventDefault();
+            store.dispatch({
+                type: 'SET_VISIBILITY_FILTER',
+                filter
+            })
+        } }>{children}</a>
     )
 }
 
-var App = ({}) => {
-    return (<div>
-        <Counter
-            state={store.getState() }
-            onIncrement={onIncrement}
-            onDecrement={onDecrement}
-            />
-        <br/>
-        <TodoList state={store} />
-    </div>
-    )
+const getVisibleTodos = (todos, filter) => {
+    switch (filter) {
+        case 'SHOW_ACTIVE':
+            return todos.filter(
+                t => t.completed 
+            );
+        case 'SHOW_COMPLETED':
+            return todos.filter(t => ! t.completed)
+        default: return todos;
+    }
+}
+
+class App extends Component {
+    render() {
+        const {
+            todos, visibilityFilter
+        } = this.props;
+        const visibleTodos = getVisibleTodos(
+            todos,
+            visibilityFilter
+        );
+        return (
+            <div>
+                <p>
+                    <FilterLink
+                        filter='SHOW_ALL'
+                        currentFilter={visibilityFilter}
+                        > All </FilterLink>
+                    <FilterLink
+                        filter='SHOW_ACTIVE'
+                        currentFilter={visibilityFilter}
+                        > Active </FilterLink>
+                    <FilterLink
+                        filter='SHOW_COMPLETED'
+                        currentFilter={visibilityFilter}
+                        > Completed </FilterLink>
+                </p>
+                <input ref={node => {
+                    this.input = node;
+                } }/>
+                <button onClick={() => {
+                    store.dispatch({
+                        type: 'ADD_TODO',
+                        text: this.input.value,
+                        id: nextTodoId++
+                    });
+                } }>Add</button>
+
+                <ul>
+                    {visibleTodos.map(todo =>
+                        <li style={{
+                            textDecoration: todo.completed ? 'line-through' : 'none'
+                        }} key={todo.id} onClick={val => {
+                            store.dispatch({
+                                type: 'TOGGLE_TODO',
+                                id: todo.id
+                            })
+                            console.log(store.getState())
+                        } }>
+                            {todo.text}
+                        </li>
+                    ) }
+                </ul>
+
+            </div>
+        )
+
+    }
 }
 
 const onIncrement = () => {
@@ -48,7 +113,7 @@ const onDecrement = () => {
 }
 var render = () => {
     ReactDom.render(
-        <App />,
+        <App {...store.getState()}/>,
         document.getElementById('app')
     );
 }
@@ -57,15 +122,8 @@ render();
 
 store.dispatch({ type: 'INCREMENT' })
 store.dispatch({ type: 'INCREMENT' })
-store.dispatch(
-    {
-        type: 'ADD_TODO',
-        text: 'foo bar',
-        id: 1
-    }
-)
 store.dispatch({
     type: 'SET_VISIBILITY_FILTER',
     filter: 'SHOW_ALL'
 })
-console.log(store.getState());
+window.store = store;
